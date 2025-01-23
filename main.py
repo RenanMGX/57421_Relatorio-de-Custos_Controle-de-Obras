ba4dd096a80fd0dfa5e3fbb20c15ef5fb2f23532
Renan Brian Hermenegildo Oliveira <renan.oliveira@patrimar.com.br>
2025-01-23 12:31:58 -0300

    correção de bug

--- main.py
+++ main.py
@@ -9,14 +9,35 @@ import os
 
 class Execute:
     @staticmethod
-    def start():
+    def start(with_sap:bool):
         paths = Paths(Config()['paths']['sharepoint'])
         
-        CJI3(date=datetime.now()).gerar_relatorios_SAP(objeto=paths)
+        if with_sap:
+            CJI3(date=datetime.now()).gerar_relatorios_SAP(objeto=paths)
         
-        Planilhas.tratar_dados(paths)
+        result = {}
+        p = os.path.join(os.getcwd(), 'Bases')
+        for file in os.listdir(p):
+            file_path = os.path.join(p, file)
+            if os.path.isfile(file_path):
+                if (c:=re.search(r'[A-z]{1}[0-9]{3}', file)):
+                    result[c.group()] = file_path
+        paths.files_extracted_from_sap = result
+        
+        print(paths.files_extracted_from_sap)
+            
+        #Planilhas.tratar_dados(paths)
+    
+    @staticmethod 
+    def start_with_sap():
+        Execute.start(with_sap=True)
+        
+    @staticmethod 
+    def start_with_not_sap():
+        Execute.start(with_sap=False)
         
 if __name__ == "__main__":
     Arguments({
-        'start': Execute.start
+        'start_with_sap': Execute.start_with_sap,
+        'start_with_not_sap': Execute.start_with_not_sap
     })
